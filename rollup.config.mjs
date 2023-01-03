0526cbe47e095bc59c2acbb2a8c70289e2a70ed1
Jon Karlsen <karlsen.jon@outlook.com>
2023-01-03 09:54:40 +0100

    FIX: rollup/tests ts collision

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
-import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup"
-import   dts            from 'rollup-plugin-dts'
+import   dts            from "rollup-plugin-dts"
+import   typescript     from "@rollup/plugin-typescript"
 
 
 const config = defineConfig( [
